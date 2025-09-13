9c29d9a1777b83167a62b0318592f43ab157b5da
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-13 00:43:50 -0700

    10 mins

--- Lab 7.cpp
+++ Lab 7.cpp
@@ -3,9 +3,25 @@
 
 #include <iostream>
 using namespace std;
+string* reverseArray(string* arr1,string* arr2,int size){
+        for(int i=0;i<size;i++){
+            
+        }
+
+
+}
+
 
 int main(){
-
+    const int size=5;
+    string* Array1 = new string[size];
+    string* Array2 = new string[size];
+    *(Array1 + 0) ="Steven";
+    *(Array1 + 0) ="Jayden";
+    *(Array1 + 0) ="Daniel";
+    *(Array1 + 0) ="Liz";
+    *(Array1 + 0) ="Kiwi";
+    reverseArray(Array1,Array2,size);
 
     return 0;
 }
