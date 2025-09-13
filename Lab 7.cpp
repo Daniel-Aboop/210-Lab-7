ec2b5c6d5de75d2f106f451536b9c76d3b07a22e
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-09-13 00:54:18 -0700

    10 mins again

--- Lab 7.cpp
+++ Lab 7.cpp
@@ -3,25 +3,38 @@
 
 #include <iostream>
 using namespace std;
-string* reverseArray(string* arr1,string* arr2,int size){
+string* reverseArray(string* arr1,int size){
         for(int i=0;i<size;i++){
-            
+            *(arr2+i)=*(arr1+(size-1-i));
+
         }
-
-
+        return arr2;
 }
 
+void displayArray(string* arr1,int size){
+
+    cout<<"Original Array: ";
+    for(int i=0;i<size;i++){
+        cout<<*(arr1 + i)<<" ";
+    }
+    cout<<endl;
+    cout<<"ReversedArray: ";
+    for(int i=0;i<size;i++){
+    cout<<*(arr2 + i)<<" ";
+    }
+    cout<<endl;
+}
 
 int main(){
     const int size=5;
     string* Array1 = new string[size];
-    string* Array2 = new string[size];
     *(Array1 + 0) ="Steven";
     *(Array1 + 0) ="Jayden";
     *(Array1 + 0) ="Daniel";
     *(Array1 + 0) ="Liz";
     *(Array1 + 0) ="Kiwi";
-    reverseArray(Array1,Array2,size);
+    reverseArray(Array1,size);
+    displayArray(Array1,size);
 
     return 0;
 }
